54d707d7f9b508e900098f831136a51fcd629778
Hiroto <hiro.f7610@gmail.com>
2021-11-16 00:53:59 +0900

    ヘッダーコンポーネントを追加

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,8 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
+  selector: 'app-root', // ここで、"app-root"と結びつけられているので、index.htmlから呼び出された時に"app.component.html"を表示させる
+  templateUrl: './app.component.html', // selectorが呼び出された時に、ここで指定したURLが表示されるように案内される
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
